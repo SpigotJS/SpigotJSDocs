fa5cecd768cfa12b16064286aa362ec10d209f56
Jamie Nevada <admin@cryogena.net>
2020-06-24 05:23:25 -0600

    Tidy up, and conform to ES6 standards.

--- index.js
+++ index.js
@@ -1,85 +1,59 @@
-var classes = [];
-
-var searchField = $("#searchfield")
-
+let classes = [];
+let searchField = $("#searchfield")
 $.getJSON("BukkitAPI.json", function(data) {
-    $("#loadingDiv").hide()
-    classes = data
-    $("#site").show()
-})
-
-searchField.on("input", function() {
-    var term = searchField.val()
-    var results = []
-    for (var k in classes) {
-        if (k.toLowerCase().includes(term.toLowerCase())) {
-            if (simpleNameIncludes(k, term)) {
-                results.push(k)
+    $("#loadingDiv").hide();
+    classes = data;
+    $("#site").show();
+});
+searchField.keydown(function(ev) {
+    if (ev.which === 13) {
+        let term = searchField.val();
+        let results = [];
+        for (let k in classes) {
+            if (k.toLowerCase().includes(term.toLowerCase())) {
+                results.push(k);
             }
         }
-    }
-    for (var k in classes) {
-        if (k.toLowerCase().includes(term.toLowerCase())) {
-            if (!simpleNameIncludes(k, term)) {
-                results.push(k)
-            }
-        }
-    }
-    var text = "<br>";
-
-    for (var i = 0; i < results.length; i++) {
-        var result = results[i]
-        var methodText = "";
-        var methods = classes[result].methods
-        for (var k in methods) {
-            var method = methods[k]
-            var paramters = []
-            for (var k2 in method) {
-                if (k2 != "returns") {
-                    paramters.push(k2)
+        let text = "<br>";
+        for (let i = 0; i < results.length; i++) {
+            let result = results[i];
+            let methodText = "";
+            let methods = classes[result].methods;
+            for (let k in methods) {
+                let method = methods[k];
+                let params = [];
+                for (let k2 in method) {
+                    if (k2 !== "returns") {
+                        params.push(k2);
+                    }
+                }
+                if (params.length === 0) {
+                    methodText += `${k}(): ${getReturnTypeName(method.returns)}<br>";
+                } else {
+                    let paramterString = getReturnTypeName(method[params[0]]) + " " + params[0];
+                    for (let i2 = 1; i2 < params.length; i2++) {
+                        let paramter = params[i2];
+                        paramterString += ", ${getReturnTypeName(method[params[i2]])} ${params[i2]}";
+                    }
+                    methodText += `${k}(${paramterString}): ${getReturnTypeName(method.returns)}<br>`;
                 }
             }
-            if (paramters.length == 0) {
-                methodText += k + "(): " + getReturnTypeName(method.returns) + "<br>"
-            } else {
-                var paramterString = getReturnTypeName(method[paramters[0]]) + " " + paramters[0]
-                for (var i2 = 1; i2 < paramters.length; i2++) {
-                    var paramter = paramters[i2]
-                    paramterString += ", " + getReturnTypeName(method[paramters[i2]]) + " " + paramters[i2]
-                }
-                methodText += k + "(" + paramterString + "): " + getReturnTypeName(method.returns) + "<br>"
-            }
-
-
+            text += `<details><summary>${result}</summary>${methodText}</details>`;
         }
-        text += "<details><summary>" + result + "</summary>" + methodText + "</details>"
+        if (!term.trim()) {
+            results = [];
+        }
+        if (results.length === 0) {
+            text = "<br> No records to show.";
+        }
+        $("#classes").html(text);
     }
-
-    if (term.trim().toLowerCase() == "") {
-        results = []
-    }
-
-    if (results.length == 0) {
-        text = "<br> No records to show."
-    }
-    $("#classes").html(text)
-})
-
-function getReturnTypeName(clazz) {
-    if (clazz == "void" || clazz == "boolean" || clazz == "int" || clazz == "double" || clazz == "float" || clazz == "char" || clazz == "byte" || clazz == "short") {
-        return clazz;
-    } else {
-        return clazz.substr(clazz.lastIndexOf(".") + 1);
-    }
-}
-
+});
+const getReturnTypeName = ((clazz) => ["void", "boolean", "int", "double", "float", "char", "byte", "short"].includes(clazz) ? clazz : clazz.substr(clazz.lastIndexOf(".") + 1));
 function simpleNameIncludes(clazz, includes) {
-    var simplename = clazz.substr(clazz.lastIndexOf(".") + 1)
-    return simplename.trim().toLowerCase() == includes.trim().toLowerCase()
+    let simplename = clazz.substr(clazz.lastIndexOf(".") + 1);
+    return simplename.trim().toLowerCase() == includes.trim().toLowerCase();
 }
-
-
-
 $("a").click(function() {
     alert("You've clicked the link.");
 });
